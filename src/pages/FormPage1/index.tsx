1c8b280d88c10a9bbf86fbddda4d4d713e7d6f2f
Tassio-Med <tassiohfmed@gmail.como>
2023-07-26 16:29:27 -0300

    feat: page1 estruturada e com funcionalidade de passar para próxima página adicionda

--- src/pages/FormPage1/index.tsx
+++ src/pages/FormPage1/index.tsx
@@ -1,8 +1,34 @@
+import { useHistory } from "react-router-dom";
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from "../../components/Theme";
 
 export const FormPage1 = () => {
+  const history = useHistory();
+  const {state, dispatch} = useForm();
+
+  const handleNextStep = () => {
+    history.push('/step2');
+  }
+
   return(
-    <div>
-      Página 1
-    </div>
+    <Theme>
+      <div>
+        <p>Passo 1/3 - {state.name}</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preeencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+
+        <label>
+          <input
+            type="text"
+            autoFocus
+            value={state}
+          />
+        </label>
+
+        <button onClick={handleNextStep}>Próximo</button>
+      </div>
+    </Theme>
   );
 }
