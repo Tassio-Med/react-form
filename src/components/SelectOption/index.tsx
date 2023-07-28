9ccaa17c5e000327bda0a4575a8d6217bd6df4b8
Tassio-Med <tassiohfmed@gmail.como>
2023-07-28 16:18:41 -0300

    feat: componente SelectOption criado com as devidas props

---
+++ src/components/SelectOption/index.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+
+export const SelectOption = ({title, description, icon}: Props) => {
+  return(
+    <div>
+      <div>{icon}</div>
+      <div>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
