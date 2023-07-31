64b79fc2201aa659ecaaad16b6c6f2f8d09be6a4
Tassio-Med <tassiohfmed@gmail.como>
2023-07-31 14:37:55 -0300

    :fire: page 3 apagada

--- src/pages/FormPage3/index.tsx
+++ src/pages/FormPage3/index.tsx
@@ -1,8 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from "../../components/Theme";
+import { ChangeEvent, useEffect } from "react";
 
 export const FormPage3 = () => {
+  const navigate = useNavigate();
+  const {state, dispatch} = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1
+    });
+  }, [dispatch]);
+
+  const handleNextStep = () => {
+    state.name !== '' ? navigate('/step2') : alert("Preencha os dados requisitados.");
+  }
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value,
+    });
+  }
+
   return(
-    <div>
-      Página 3
-    </div>
+    <Theme>
+      <div>
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preeencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+
+        <label>
+          <input
+            type="text"
+            autoFocus
+            value={state.name}
+            onChange={handleNameChange}
+          />
+        </label>
+
+        <button onClick={handleNextStep}>Próximo</button>
+      </div>
+    </Theme>
   );
 }
