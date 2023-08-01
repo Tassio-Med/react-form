4da4a2042a42df13ca4fd3360819cf3007304abe
Tassio-Med <tassiohfmed@gmail.como>
2023-08-01 16:34:27 -0300

    build: adicionada estilização com material=ui

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react"
 import { Header } from "../Header"
 import { SidebarItem } from "../SidebarItem"
+import { Box, Container, Paper, Grid } from "@mui/material";
+
 
 type Props = {
   children: ReactNode
@@ -8,35 +10,41 @@ type Props = {
 
 export const Theme = ({children}: Props) => {
   return (
-    <div>
-      <div>
-        <Header/>
-        <div>
-          <div>
-            <SidebarItem
-              title="Pessoa"
-              description="Se identifique"
-              icon="profile"
-              path="/"
-            />
-            <SidebarItem
-              title="Profissional"
-              description="Seu nível"
-              icon="book"
-              path="/page2"
-            />
-            <SidebarItem
-              title="Contatos"
-              description="Como te achar"
-              icon="mail"
-              path="/page3"
-            />
-          </div>
-          <div>
-            {children}
-          </div>
-        </div>
-      </div>
-    </div>
+    <Box bgcolor="#02044A" color="#FFF" minHeight="100vh">
+      <Container maxWidth="lg">
+        <Grid container>
+          <Header/>
+          <Grid container>
+            <Grid item xs={3}>
+              <Paper elevation={3} style={{ padding: '20px' }}>
+                <SidebarItem
+                  title="Pessoa"
+                  description="Se identifique"
+                  icon="profile"
+                  path="/"
+                />
+                <SidebarItem
+                  title="Profissional"
+                  description="Seu nível"
+                  icon="book"
+                  path="/page2"
+                />
+                <SidebarItem
+                  title="Contatos"
+                  description="Como te achar"
+                  icon="mail"
+                  path="/page3"
+                />
+              </Paper>
+            </Grid>
+            <Grid item xs={9}>
+              <Paper elevation={3} style={{ padding: '40px' }}>
+                {children}
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
