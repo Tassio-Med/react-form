33d6c8e321fba9bf5f42f9e2ebe3467cf91f1e65
Tassio-Med <tassiohfmed@gmail.como>
2023-08-01 15:15:56 -0300

    feat: componente sidebar importado ao theme

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react"
 import { Header } from "../Header"
+import { SidebarItem } from "../SidebarItem"
+
 type Props = {
   children: ReactNode
 }
@@ -11,7 +13,24 @@ export const Theme = ({children}: Props) => {
         <Header/>
         <div>
           <div>
-            ...
+            <SidebarItem
+              title="Pessoa"
+              description="Se identifique"
+              icon="profile"
+              path="/"
+            />
+            <SidebarItem
+              title="Profissional"
+              description="Seu nível"
+              icon="book"
+              path="/page2"
+            />
+            <SidebarItem
+              title="Contatos"
+              description="Como te achar"
+              icon="mail"
+              path="/page3"
+            />
           </div>
           <div>
             {children}
