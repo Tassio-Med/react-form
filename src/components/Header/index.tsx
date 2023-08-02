3ba6020585c05b2c9ddbad6685d2bd74cc44976a
Tassio-Med <tassiohfmed@gmail.como>
2023-08-02 15:30:33 -0300

    feat: adicionada estilização ao header com mui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
+import { Typography } from "@mui/material";
+
 export const Header = () => {
   return (
-    <div>
-      <h1>Cadastro de Desenvolvedor</h1>
-      <p>Faça seu cadastro no formulário e seja visto pelos Tech Recruiters!</p>
+    <div  style={{ padding: "25px 0", borderBottom: "1px solid #16195C" }}>
+      <Typography variant="h1" style={{ margin: 0, padding: 0, fontSize: 28 }}>
+        Cadastro de Desenvolvedor
+      </Typography>
+      <Typography variant="body1" style={{ fontSize: 14, color: "#B8B8D4" }}>
+        Faça seu cadastro no formulário e seja visto pelos Tech Recruiters!
+      </Typography>
     </div>
   )
 }
