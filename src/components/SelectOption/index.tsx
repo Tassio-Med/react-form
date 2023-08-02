ca078ec2dc822ab7fb2c46bf1a39e6e488439bd1
Tassio-Med <tassiohfmed@gmail.como>
2023-08-02 16:12:23 -0300

    feat: adicionada estilização ao SelectOption com mui

--- src/components/SelectOption/index.tsx
+++ src/components/SelectOption/index.tsx
@@ -1,3 +1,5 @@
+import { Box, Typography } from "@mui/material";
+
 type Props = {
   title: string;
   description: string;
@@ -7,12 +9,39 @@ type Props = {
 
 export const SelectOption = ({title, description, icon}: Props) => {
   return(
-    <div >
-      <div>{icon}</div>
-      <div>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </div>
-    </div>
+    <Box
+      style={{
+        display: "flex",
+        border: "2px solid #16195C",
+        borderRadius: "10px",
+        padding: "20px",
+        marginBottom: "15px",
+        alignItems: "center",
+        cursor: "pointer",
+      }}
+    >
+      <Box
+        style={{
+          width: "60px",
+          height: "60px",
+          borderRadius: "50%",
+          backgroundColor: "#191A59",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "25px",
+        }}
+      >
+        {icon}
+      </Box>
+      <Box style={{ flex: 1, marginLeft: "20px" }}>
+        <Typography style={{ fontSize: "17px", fontWeight: "bold", marginBottom: "7px" }}>
+          {title}
+        </Typography>
+        <Typography style={{ fontSize: "14px", color: "#B8B8D4" }}>
+          {description}
+        </Typography>
+      </Box>
+    </Box>
   );
-}
+} 
