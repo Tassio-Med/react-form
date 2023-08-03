771e32b80d97207f8d0265bcd54474f68a7cb503
Tassio-Med <tassiohfmed@gmail.como>
2023-08-03 14:54:23 -0300

    feat: adicionada previamente a estilização com mui no sidebar, mas ainda não está pronta

--- src/components/SidebarItem/index.tsx
+++ src/components/SidebarItem/index.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import { Box, Typography } from '@mui/material';
+import { Person as ProfileIcon, Book as BookIcon, Mail as MailIcon } from '@mui/icons-material';
+
 
 type Props = {
   title: string;
@@ -9,17 +12,19 @@ type Props = {
 
 export const SidebarItem = ({title, description, icon, path}: Props) => {
   return (
-    <div>
+    <Box>
       <Link to={path}>
-        <div>
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
-        <div>
-          {/* Área do ícone */}
-        </div>
+        <Box>
+          <Typography>{title}</Typography>
+          <Typography>{description}</Typography>
+        </Box>
+        <Box>
+          {icon === "profile" && <ProfileIcon style={{ fill: "white", width: 24, height: 24 }} />}
+          {icon === "book" && <BookIcon style={{ fill: "white", width: 24, height: 24 }} />}
+          {icon === "mail" && <MailIcon style={{ fill: "white", width: 24, height: 24 }} />}
+        </Box>
         
       </Link>
-    </div>
+    </Box>
   );
 }
